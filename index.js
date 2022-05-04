d028b36c5d9fde97bd2c6bb635b6f3a5ca8ecfe8
Jean Pierre <yvens47@yahoo.com>
2022-05-04 10:36:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const PORT  = process.env.PORT || 5000;
+
+
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(bodyParser());
+
+// routes
+
+app.get('/', (req, res)=>{
+  res.json({message:"Invoice Management System"});
+})
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=>console.log("App has started..."))
+
+
+
