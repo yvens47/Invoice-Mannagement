b56c78d6320e43ac1fafa688fd8995b762630c5f
Jean Pierre <yvens47@yahoo.com>
2022-05-04 12:49:41 +0000

    register controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,62 +1,75 @@
 
 const User = require('../models/user.model')
 
-const login = Model =>(req, res)=>{
+const login = Model => (req, res) => {
 
-  const {email, password} = req.body
+  const { email, password } = req.body
   // no empty email and password
 
 
 
   try {
-     res.json({email, password})   ; 
+    res.json({ email, password });
   }
-  catch(e){
+  catch (e) {
 
     console.log(e);
-    
+
   }
 
- 
-  
+
+
 }
 
-const register = Model =>(req, res)=>{
+const register = Model => (req, res) => {
 
-  try{
-    
+  try {
 
-    res.json({login:"register Controller"})
+    const user = new Tank(req.body);
+    user.save(function(err) {
+      if (err) {
+        console.log(err)
+        res.json({ error: err.message });
+      }
+      // saved!
+
+      res.json({ success: true, message: "user created successfully" })
+
+    });
+
+
+
+
   }
-  catch(e){
+  catch (e) {
     console.log(e)
   }
 
-  
-  
+
+
 }
 
-const forgotPassword = Model =>(req, res)=>{
+const forgotPassword = Model => (req, res) => {
 
-  res.json({login:"Login Controller"})
-  
+  res.json({ login: "Login Controller" })
+
 }
 
-const isAuthorized = (req, res)=>{
+const isAuthorized = (req, res) => {
 
-  res.json({login:"Login Controller"})
-  
+  res.json({ login: "Login Controller" })
+
 }
 
 
 
 
 
-module.exports ={
-  login:login(User),
-  register:register(User),
+module.exports = {
+  login: login(User),
+  register: register(User),
   isAuthorized,
-  forgotPassword:forgotPassword({})
-  
-  
+  forgotPassword: forgotPassword({})
+
+
 }
